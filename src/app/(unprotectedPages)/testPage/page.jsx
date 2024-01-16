3f7268eb48c8618e544498f615531b5b96b2e520
jiitra <74521649+jiitra@users.noreply.github.com>
2024-01-16 13:51:51 -0500

    :sparkles: Implemented all header & para styles

--- src/app/(unprotectedPages)/testPage/page.jsx
+++ src/app/(unprotectedPages)/testPage/page.jsx
@@ -6,8 +6,19 @@ import { db } from "@/lib/firebase";
 const Index = () => {
   return (
     <div>
-      <h1>Test page</h1>
-      <p>use this as a playground to test components/styling</p>
+      <h1>
+        Test <i>page</i>
+      </h1>
+      <h2>Heading 2</h2>
+      <h3>Heading 3</h3>
+      <h4>Heading 4</h4>
+      <h5>Heading 5</h5>
+      <p>
+        use this as a playground to test <i>components/styling</i>
+      </p>
+      <p class="semibold">use this as a playground to test components/styling</p>
+      <p class="bold">use this as a playground to test components/styling</p>
+      <p class="large">this is large paragraph</p>
     </div>
   );
 };
