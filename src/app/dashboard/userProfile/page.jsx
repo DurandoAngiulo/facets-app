1abb38946ce107aa046a92efa1f38191579828cf
DurandoAngiulo <87902832+DurandoAngiulo@users.noreply.github.com>
2024-02-02 13:04:26 -0500

    userProfile page made

--- src/app/dashboard/userProfile/page.jsx
+++ src/app/dashboard/userProfile/page.jsx
@@ -1,13 +1,51 @@
 "use client";
-
-import React, { useEffect, useState } from "react";
+import { useState, useEffect } from "react";
 import { useAuth } from "@/context/AuthContext";
 import DashboardLayout from "@/components/layouts/DashboardLayout";
+import { usePathname, useRouter } from "next/navigation";
+import { extractIdFromUrl } from "@/utils/util-functions";
+import { getProfileById } from "@/services/profile-service";
+import { calculateAge } from "@/utils/util-functions.js";
 
 const Index = () => {
+  const { currentUser } = useAuth();
+  const pathname = usePathname();
+  const router = useRouter();
+  const profileId = currentUser?.uid;
+  const [profileInformation, setProfileInformation] = useState("");
+  console.log(currentUser);
+  useEffect(() => {
+    const fetchProfile = async (profileId) => {
+      try {
+        const profileResult = await getProfileById(profileId);
+        let profileData = profileResult?.data;
+
+        console.log(profileData);
+        setProfileInformation(profileData);
+      } catch (error) {
+        console.error("Failed to fetch user profile:", error);
+      }
+    };
+
+    fetchProfile(profileId);
+  }, [currentUser]); // Empty dependency array to run only on component mount
+
+  console.log(profileInformation, "proInfo");
+
   return (
     <DashboardLayout>
-      <div>Main User profile</div>
+      <div>
+        <h2>all unstyled profile data</h2>
+        <p>{profileInformation?.data?.firstName}</p>
+        <p>{calculateAge(profileInformation?.data?.birthday)}</p>
+        <p>{profileInformation?.data?.bio}</p>
+        <p>{profileInformation?.data?.location}</p>
+        <p>{profileInformation?.data?.occupation}</p>
+        <p>{profileInformation?.data?.pronouns}</p>
+        <p>personalFacetprompts placeholder TBD</p>
+        <p>friendFacetPlaceholder TBD</p>
+        <p>image palcehodler TBD</p>
+      </div>
     </DashboardLayout>
   );
 };
