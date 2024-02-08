f6776d14d21966e4acd3bb01ad3158599202b109
mrpaulphan <paul@mrpaulphan.com>
2024-02-08 12:57:47 -0500

    test

--- src/app/dashboard/layout.js
+++ src/app/dashboard/layout.js
@@ -3,6 +3,10 @@
 import Navbar from "@/components/Navbar";
 import withProtectedRoutes from "@/components/layouts/withProtectedRoutes";
 
+/**
+ * Dashboard Layout component
+ * @param {{ children: React.ReactNode }} props - The component props
+ */
 const DashboardLayout = ({ children }) => {
   return (
     <>
