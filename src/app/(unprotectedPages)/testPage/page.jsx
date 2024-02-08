ca0180bd355cc2f2d2cdcc013d7585ce2908742b
jiitra <74521649+jiitra@users.noreply.github.com>
2024-02-08 17:38:57 -0500

    will this not break vercel?

--- src/app/(unprotectedPages)/testPage/page.jsx
+++ src/app/(unprotectedPages)/testPage/page.jsx
@@ -2,9 +2,6 @@
 import { collection, doc, setDoc } from "firebase/firestore";
 import { db } from "@/lib/firebase";
 import Icon from "@/components/Icon";
-import RadioInput from "@/components/Inputs/RadioInput";
-import TextInput from "@/components/Inputs/TextInput";
-import Checkbox from "@/components/Inputs/Checkbox";
 
 const Index = () => {
   return (
@@ -13,9 +10,6 @@ const Index = () => {
       <p>use this as a playground to test components/styling</p>
       <div className="flex flex-col gap-2 px-10">
         {/* <Icon iconName="next" className="w-full fill-slate-900" /> */}
-        <RadioInput label="hello" />
-        <Checkbox label="hello again" />
-        <TextInput placeholder="test" />
       </div>
     </div>
   );
