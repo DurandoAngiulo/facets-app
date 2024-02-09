de2256487bccfd90d2a2043f721e9b3a88ec7d43
nytrea <anoushsan@gmail.com>
2024-02-09 16:45:39 -0500

    link and structure
    link for continue button and additional css for the page

--- src/components/onboarding/EnterBirthday.jsx
+++ src/components/onboarding/EnterBirthday.jsx
@@ -24,22 +24,22 @@ export const EnterBirthday = ({ handleUpdateProfile }) => {
       <ProgressBar width="20%" step="2"></ProgressBar>
       <form onSubmit={handleSubmit}>
         {/* TODO: make birthday formatting */}
-        <div className="w-full h-6 relative">
-          <Icon iconName="cake" className="w-full h-6 left-0 top-9 bottom-9 absolute" />
+        <div className="w-full h-4 relative mt-12">
+          <Icon iconName="cake" className="w-full h-8 left-0 top-6 bottom-6 absolute" />
         </div>
-        <h1 style={{ color: "var(--brand)" }} className="my-10 w-full text-center">
+        <h1 style={{ color: "var(--brand)" }} className="my-14 w-full text-center">
           What is your birthday?
           <p style={{ color: "var(--text)" }}>
             <input
               type="date"
-              className="text-black border-solid w-full h-14 rounded border border-zinc-500 border-opacity-50 my-7"
+              className="text-black border-solid w-full h-14 rounded border border-zinc-500 border-opacity-50 p-4 my-8"
               value={birthday}
               onChange={(e) => setBirthday(e.target.value)}
             />
           </p>
         </h1>
-        <button id="birthday-continue" type="submit">
-          Continue
+        <button className="w-full leading-none align-bottom " id="birthday-continue" type="submit">
+          <PrimaryButton active="true" label="Continue"></PrimaryButton>
         </button>
       </form>
       {error && <p style={{ color: "red" }}>{error}</p>}
