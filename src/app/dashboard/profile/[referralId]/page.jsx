f89cba10472c2ead94d8d8ff5280f7a01e8f13d1
DurandoAngiulo <87902832+DurandoAngiulo@users.noreply.github.com>
2024-02-26 20:09:47 -0500

    hot fix with images

--- src/app/dashboard/profile/[referralId]/page.jsx
+++ src/app/dashboard/profile/[referralId]/page.jsx
@@ -1,6 +1,5 @@
 "use client";
 
-import { extractIdFromUrl, replaceNameInString } from "@/utils/util-functions";
 import { useEffect, useState } from "react";
 
 import BeveledContainer from "@/components/BeveledContainer/Index";
@@ -8,55 +7,21 @@ import Icon from "@/components/Icon";
 import MaskedImage from "@/components/MaskedImage/Index";
 import { useAuth } from "@/context/AuthContext";
 import { transformUserFacets } from "@/services/facet-services";
-import { getProfileById } from "@/services/profile-service";
-import { calculateAge } from "@/utils/util-functions";
+import { calculateAge, replaceNameInString, extractIdFromUrl } from "@/utils/util-functions";
 import { usePathname } from "next/navigation";
 import MoreDetailsPillGroup from "@/components/MoreDetailsPillGroup/Index.jsx";
+import Link from "next/link";
+import ROUTES from "@/constants/routes";
+import FacetsList from "@/components/FacetsList";
+import { getProfileById } from "@/services/profile-service";
 
 const Index = () => {
   const { currentUser } = useAuth();
   const pathname = usePathname();
-  const profileId = extractIdFromUrl(pathname);
-  const [profileInformation, setProfileInformation] = useState("");
-
-  const FacetGroupCard = ({ facet }) => {
-    const FacetCard = ({ response }) => {
-      return (
-        <li key={response.id} className="flex flex-col gap-4 snap-center">
-          <MaskedImage
-            height={292}
-            width={292}
-            image="https://imgv3.fotor.com/images/blog-richtext-image/take-a-selfie-with-friends.jpg"
-          />
-          <BeveledContainer>
-            <p style={{ color: "var(--text)" }}>
-              <i>{replaceNameInString(response.prompt, profileInformation?.firstName)}</i>
-            </p>
-            <p className="semibold" style={{ fontSize: "var(--font-size-p-md)", color: "var(--brand)" }}>
-              {response.response}
-            </p>
-            <div className="absolute bottom-4 right-4">
-              <Icon className="h-7 w-7" iconName="messageDots" />
-            </div>
-          </BeveledContainer>
-        </li>
-      );
-    };
-
-    return (
-      <div className="mt-3 flex flex-col gap-2 snap-center">
-        <p className="text-center" style={{ fontSize: "var(--font-size-p-md)", color: "var(--text)" }}>
-          Facet by{" "}
-          <b>{facet.friendshipPeriod ? `a friend of ${facet.friendshipPeriod}` : profileInformation?.firstName}</b> {}
-        </p>
-        <ul className="flex flex-col gap-2">
-          {facet.responses.map((response) => (
-            <FacetCard key={response.prompt_id} response={response} />
-          ))}
-        </ul>
-      </div>
-    );
-  };
+  const profileId = extractIdFromUrl(pathname); // You need to define this function
+  const [profileInformation, setProfileInformation] = useState(null);
+  const friendFacetsExist = profileInformation?.friendFacets && profileInformation.friendFacets.length > 0;
+  const profileFacetsExist = profileInformation?.personalFacets && profileInformation.personalFacets[0] !== undefined;
 
   useEffect(() => {
     const fetchProfile = async (profileId) => {
@@ -69,7 +34,7 @@ const Index = () => {
       }
     };
     fetchProfile(profileId);
-  }, []);
+  }, [profileId]);
 
   useEffect(() => {
     if (!profileInformation) return;
@@ -80,56 +45,48 @@ const Index = () => {
     };
 
     transformFacetData();
-  }, [JSON.stringify(profileInformation)]);
+  }, [profileInformation]);
+
+  if (!profileInformation) {
+    return <h1>Loading...</h1>;
+  }
 
   return (
     <>
-      <div className="page">
-        <div style={{ background: "#fff" }} className="pb-8">
-          <div className="w-full justify-center items-center gap-2 inline-flex flex-wrap px-8">
-            {/* header with back button, name, and kabob with modal options */}
-            <div className="w-full inline-flex pt-8">
-              {/* <Link href={`${ROUTES.FEED.path}`}> */}
-              <Icon className="w-6 h-6 justify-between items-center flex" iconName="backArrow"></Icon>
-              {/* </Link> */}
-              <h2 className="w-full flex-col justify-start items-center inline-flex gradient-text">
-                {profileInformation?.firstName}
-              </h2>
-              <Icon className="w-6 h-6 origin-top-left justify-between items-center flex" iconName="kabob"></Icon>
+      <div>
+        <header>
+          <Link href={ROUTES.EDIT_PROFILE.path}>
+            <h2>edit profile</h2>
+          </Link>
+
+          <h2>all unstyled profile data</h2>
+          <p>{profileInformation?.firstName}</p>
+          <p>{calculateAge(profileInformation?.birthday)}</p>
+          <p>{profileInformation?.bio}</p>
+          <p>{profileInformation?.location}</p>
+          <p>{profileInformation?.occupation}</p>
+          <p>{profileInformation?.pronouns}</p>
+        </header>
+        <MoreDetailsPillGroup moreDetails={profileInformation?.moreDetails} />
+
+        <section className="bg-green-400">
+          {profileFacetsExist && (
+            <div>
+              <h3>Facet By {profileInformation?.firstName}</h3>
+              <ul>
+                <FacetsList facet={profileInformation.personalFacets[0]} />
+              </ul>
             </div>
-          </div>
+          )}
 
-          <p style={{ color: "var(--text)" }} className="text-center italic">
-            {profileInformation?.pronouns}
-          </p>
-
-          <div className="w-full justify-center items-center gap-1 inline-flex my-1">
-            <p style={{ color: "var(--text)" }}>{calculateAge(profileInformation?.birthday)}</p>
-            <Icon iconName="diamondBio" className="w-[12px] h-[12px]" />
-            <p style={{ color: "var(--text)" }}>{profileInformation?.occupation}</p>
-            <Icon iconName="diamondBio" className="w-[12px] h-[12px]" />
-            <p style={{ color: "var(--text)" }}>{profileInformation?.location}</p>
-          </div>
-
-          <MoreDetailsPillGroup moreDetails={profileInformation?.moreDetails} />
-
-          <p className="pt-2 px-8"> {profileInformation?.bio} </p>
-        </div>
-
-        <div
-          className="flex flex-row overflow-auto gap-5 px-8 snap-proximity snap-x"
-          style={{ background: "var(--background-gradient-lr" }}
-        >
-          <div className="mb-24 ">
-            {profileInformation?.personalFacet?.map((facet) => (
-              <FacetGroupCard key={facet.id} facet={facet} />
+          {friendFacetsExist &&
+            profileInformation.friendFacets.map((facet) => (
+              <div key={facet.respondantUserId}>
+                <h3>Facet By A friend of {facet.friendshipPeriod}</h3>
+                <FacetsList facet={facet} currentProfile={profileInformation} />
+              </div>
             ))}
-          </div>
-
-          {profileInformation?.friendFacets?.map((facet) => (
-            <FacetGroupCard key={facet.id} facet={facet} />
-          ))}
-        </div>
+        </section>
       </div>
     </>
   );
