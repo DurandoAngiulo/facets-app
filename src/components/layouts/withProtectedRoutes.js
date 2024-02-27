491882b689ba65d010b1adb52240b7122c307eda
mrpaulphan <paul@mrpaulphan.com>
2024-02-27 16:44:14 -0500

    ¯\_(ツ)_/¯

--- src/components/layouts/withProtectedRoutes.js
+++ src/components/layouts/withProtectedRoutes.js
@@ -2,6 +2,7 @@ import { usePathname, useRouter } from "next/navigation";
 
 import ROUTES from "@/constants/routes";
 import { useAuth } from "@/context/AuthContext";
+import { useEffect } from "react";
 
 /**
  * withProtectedRoutes HOC for route protection
@@ -16,37 +17,41 @@ const withProtectedRoutes = (ComponentToWrap) => {
     const pathname = usePathname();
     const isAdminRoute = pathname.startsWith("/admin");
     const isOnboardingRoute = pathname.includes("/onboarding");
-    const profileCreationRoute = `${ROUTES.ONBOARDING.path}/profile-creation`;
+    const profileCreationRoute = ROUTES.PROFILE_CREATION.path;
 
-    if (loading && !currentUser) {
+    useEffect(() => {
+      if (loading && !currentUser) {
+        return <div>Loading...</div>;
+      }
+
+      if (!currentUser) {
+        router.push(`${ROUTES.LOGIN.path}?redirectTo=${pathname}`);
+        return;
+      }
+
+      if (isAdminRoute && userRole !== "admin") {
+        router.push(`${ROUTES.UNAUTHORIZED.path}`);
+        return;
+      }
+
+      if (
+        currentUser?.profile?.onboardingStatus === "inProgress" &&
+        !isOnboardingRoute &&
+        profileCreationRoute !== pathname
+      ) {
+        router.push(ROUTES.PROFILE_CREATION.path);
+        return;
+      }
+
+      if (currentUser?.profile?.onboardingStatus === "complete" && isOnboardingRoute) {
+        router.push(ROUTES.FEED.path);
+        return;
+      }
+    }, [currentUser, loading, pathname, router, userRole, isAdminRoute, isOnboardingRoute, profileCreationRoute]);
+
+    if (loading) {
       return <div>Loading...</div>;
     }
-
-    if (!currentUser) {
-      router.push(`${ROUTES.LOGIN.path}?redirectTo=${pathname}`);
-      return;
-    }
-
-    if (isAdminRoute && userRole !== "admin") {
-      router.push(`${ROUTES.UNAUTHORIZED.path}`);
-      return;
-    }
-
-    if (
-      currentUser?.profile?.onboardingStatus === "inProgress" &&
-      !isOnboardingRoute &&
-      profileCreationRoute !== pathname
-    ) {
-      router.push(`${ROUTES.ONBOARDING.path}/profile-creation`);
-      // redirect to onboarding screen
-      // router.push(`${ROUTES.UNAUTHORIZED.path}`);
-      return;
-    }
-
-    if (currentUser?.profile?.onboardingStatus === "complete" && isOnboardingRoute) {
-      router.push(`${ROUTES.DASHBOARD.path}/feed`);
-    }
-    // redirect to dashboard main page
 
     return <ComponentToWrap {...props} />;
   };
