c7a729ae7a1aa6e8b48eaf6ee70367495a8b62f9
nytrea <anoushsan@gmail.com>
2024-02-28 18:30:58 -0500

    my details page updates

--- src/app/dashboard/userProfile/edit/page.jsx
+++ src/app/dashboard/userProfile/edit/page.jsx
@@ -25,32 +25,114 @@ const Index = () => {
 
         <hr className="my-4" style={{ borderColor: "var(--border)" }}></hr>
 
-        <div className="inline-flex w-full p-3 my-2 rounded border border-zinc-500 border-opacity-50">
-          <p className="w-5/6 font-medium">Account Information</p>
-          {/* change backarrow to forwardarrow or smn idk */}
-          <Icon iconName="backArrow" className="w-[30px] h-[30px] rotate-180 m-auto" />
+        <h3 style={{ color: "var(--brand)" }}>Primary Information</h3>
+        <div>
+          <Link href="/dashboard/editAction/name">
+            <p>Name</p>
+          </Link>
+          <Link href="/dashboard/editAction/birthday">
+            <p>Birthday</p>
+          </Link>
+          <Link href="/dashboard/editAction/pronouns">
+            <p>pronouns</p>
+          </Link>
+          <Link href="/dashboard/editAction/gender">
+            <p>Gender</p>
+          </Link>
+          <Link href="/dashboard/editAction/datingPreferences">
+            <p>Dating preferences</p>
+          </Link>
+          <Link href="dashboard/editAction/ageRange">
+            <p>age range</p>
+          </Link>
+          <Link href="/dashboard/editAction/occupation">
+            <p>Occupation</p>
+          </Link>
+          <Link href="/dashboard/editAction/location">
+            <p>Location</p>
+          </Link>
         </div>
 
+        <h3 style={{ color: "var(--brand)" }}>More Details</h3>
         <div className="inline-flex w-full p-3 my-2 rounded border border-zinc-500 border-opacity-50">
           <p className="w-5/6 font-medium my-auto">Add More Details</p>
-          {/* change backarrow to forwardarrow or smn idk */}
           <Link href="/dashboard/editAction/moreDetails" className="m-auto flex">
             <Icon iconName="backArrow" className="w-[30px] h-[30px] rotate-180 m-auto" />
           </Link>
         </div>
 
+        <h3 style={{ color: "var(--brand)" }}>Facet Photos</h3>
+        <div className="inline-flex w-full p-3 my-2 rounded border border-zinc-500 border-opacity-50">
+          {/* change backarrow to forwardarrow or smn idk */}
+          <p className="w-5/6 font-medium my-auto">Photos</p>
+          <Link href="/dashboard/editAction/uploadPhotos" className="m-auto flex">
+            <Icon iconName="backArrow" className="w-[30px] h-[30px] rotate-180 m-auto" />
+          </Link>
+        </div>
+
+        {/* <div className="inline-flex w-full p-3 my-2 rounded border border-zinc-500 border-opacity-50">
+          <p className="w-5/6 font-medium">Account Information</p>
+          <Icon iconName="backArrow" className="w-[30px] h-[30px] rotate-180 m-auto" />
+        </div>
+
         <div className="inline-flex w-full p-3 my-2 rounded border border-zinc-500 border-opacity-50">
           <p className="w-5/6 font-medium">Edit Facets Summary</p>
-          {/* change backarrow to forwardarrow or smn idk */}
           <Icon iconName="backArrow" className="w-[30px] h-[30px] rotate-180 m-auto" />
         </div>
 
         <div className="inline-flex w-full p-3 my-2 rounded border border-zinc-500 border-opacity-50">
           <p className="w-5/6 font-medium">Facets Management</p>
-          {/* change backarrow to forwardarrow or smn idk */}
           <Icon iconName="backArrow" className="w-[30px] h-[30px] rotate-180 m-auto" />
-        </div>
+        </div> */}
       </div>
+
+      {/* <div>
+        <h2>edit</h2>
+        <Link href="/dashboard/userProfile">go back</Link>
+        <h3>primary info</h3>
+        <div>
+          <Link href="/dashboard/editAction/name">
+            <p>Name</p>
+          </Link>
+          <Link href="/dashboard/editAction/birthday">
+            <p>Birthday</p>
+          </Link>
+          <Link href="/dashboard/editAction/pronouns">
+            <p>pronouns</p>
+          </Link>
+          <Link href="/dashboard/editAction/gender">
+            <p>Gender</p>
+          </Link>
+          <Link href="/dashboard/editAction/datingPreferences">
+            <p>Dating preferences</p>
+          </Link>
+          <Link href="dashboard/editAction/ageRange">
+            <p>age range</p>
+          </Link>
+          <Link href="/dashboard/editAction/occupation">
+            <p>Occupation</p>
+          </Link>
+          <Link href="/dashboard/editAction/location">
+            <p>Location</p>
+          </Link>
+        </div>
+        <h3>More details section</h3>
+        <div>
+          <Link href="/dashboard/editAction/moreDetails">
+            <p>More Details</p>
+          </Link>
+        </div>
+        <h3>Photo upload section</h3>
+        <div>
+          <Link href="/dashboard/editAction/uploadPhotos">
+            <p>photos</p>
+          </Link>
+        </div>
+        <h3>summary card section?</h3>
+        <div>
+          <p>not sure how this looks</p>
+        </div>
+      </div> */}
     </DashboardLayout>
   );
 };
